7bf713376cb9299c16e4e619c68d365ae681c946
Korat Dishant <koratdishant536631@gmail.com>
2024-01-23 20:20:08 +0530

    commit

--- controller.py
+++ controller.py
@@ -6,16 +6,11 @@ pyautogui.keyUp('left')
 def signal(keyPressed=""):
     print("key pressed " , keyPressed)
     if keyPressed == "left" : 
-        pyautogui.keyDown('right')
-        pyautogui.keyUp('left')
+        pyautogui.press('left')
  
     if keyPressed == "right" : 
-        pyautogui.keyDown('left')
-        pyautogui.keyUp('right')
+        pyautogui.press('right')
 
-    if keyPressed == "s" : 
-        pyautogui.keyUp('right')
-        pyautogui.keyUp('left')
  
 
  
